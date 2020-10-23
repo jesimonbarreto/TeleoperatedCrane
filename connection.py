136ea42e5e6c0aa49c7e50aa77ed2d66de44ba91
Ariel <ariel.f.m.domingues@gmail.com>
2020-10-22 21:53:35 -0300

    Update GUI, add functionalities and optmize motion functions

--- connection.py
+++ connection.py
@@ -9,6 +9,12 @@ try:
 except:
     print ('Error import sim. Verify files and settings Coppelia.')
 
+def radian2degree(angle):
+    return 180*angle/math.pi
+
+def degree2radian(angle):
+    return math.pi*angle/180
+
 class Connection(object):
     def __init__(self, radius=7.5, showIm=False):
         self.showImage = showIm
@@ -18,6 +24,8 @@ class Connection(object):
         self.distanceClaw = 2*math.pi*radius #0 a 360 verificar tamanho total e fazer a conta de comprimento 2*pi*r pra direita aumenta x. Pra esquerda diminui x se chegar ao total volta a 0 se for negativo vai pra 360
         self.currentDistanceClaw = 0  
         self.currentAngleClaw = 0
+        self.currentHeightHook = 0
+
     def init_connection(self, ip = '127.0.0.1' , port = 19997):
         #print ('Program started')
         sim.simxFinish(-1) # just in case, close all opened connections
@@ -26,6 +34,7 @@ class Connection(object):
             return -1, False
         self.clientID = clientID
         self.connectionStatus = True
+
         # Get Coppelia Objects ID
         self.boom =  sim.simxGetObjectHandle(clientID,'Atuador_braco',sim.simx_opmode_blocking)[-1]
         self.claw = sim.simxGetObjectHandle(clientID,'Atuador_garra',sim.simx_opmode_blocking)[-1] 
@@ -35,46 +44,79 @@ class Connection(object):
         self.proximity_sensor = sim.simxGetObjectHandle(clientID,'Proximity_sensor',sim.simx_opmode_blocking)[-1]
         self.err_code,resolution,image = sim.simxGetVisionSensorImage(clientID,self.cam,0,sim.simx_opmode_streaming)
         self.status = sim.simxReadProximitySensor(clientID,self.proximity_sensor,sim.simx_opmode_streaming)[1]
+        self.boomStructure = sim.simxGetObjectHandle(clientID,'Braco',sim.simx_opmode_blocking)[-1]
+        
+        # Inicia simulacao
+        sim.simxStartSimulation(self.clientID, sim.simx_opmode_blocking)
+
 
         #if self.showImage:
             # Enables interactive mode for imshow
             #mpl.ion()
         
         return clientID, self.connectionStatus
-    
-    def convertDistAngle(self, step):
-        stepT = step % self.distanceClaw
-        self.currentDistanceClaw += stepT
-        if self.currentDistanceClaw < 0:
-            self.currentDistanceClaw+=self.distanceClaw
-        elif self.currentDistanceClaw > self.distanceClaw:
-            self.currentDistanceClaw-=self.distanceClaw
+
+    # def convertDistAngle(self, step):
+    #     stepT = step % self.distanceClaw
+    #     self.currentDistanceClaw += stepT
+    #     if self.currentDistanceClaw < 0:
+    #         self.currentDistanceClaw+=self.distanceClaw
+    #     elif self.currentDistanceClaw > self.distanceClaw:
+    #         self.currentDistanceClaw-=self.distanceClaw
         
-        self.currentAngleClaw = 360 * self.currentDistanceClaw / self.distanceClaw
+    #     self.currentAngleClaw = 360 * self.currentDistanceClaw / self.distanceClaw
 
-        return self.currentAngleClaw
+    #     return self.currentAngleClaw
 
+
+    # "Liga" guindaste
+    def commandCraneOnOff(self):
+        if self.connectionStatus:
+            self.craneStatus= not self.craneStatus
+        return self.craneStatus
+
+    def getCraneStatus(self):
+        return self.craneStatus
+
+    # Liga ima
+    def commandMagnetOnOff(self):
+        if self.craneStatus and self.connectionStatus:
+            sim.simxCallScriptFunction(self.clientID,'Base',sim.sim_scripttype_childscript,'AtuadorIma',[],[],[],bytearray(),sim.simx_opmode_blocking)
+            self.magnetStatus = not self.magnetStatus
+        return self.magnetStatus
+
+    def getMagnetStatus(self):
+        return self.magnetStatus
+
+    # Atuador braco
     def actionBoom(self, step):
         if self.craneStatus and self.connectionStatus:
-            step *= 0.1
-            sim.simxSetJointTargetVelocity(self.clientID,self.boom,step,sim.simx_opmode_oneshot)
-            time.sleep(0.2)
-            sim.simxSetJointTargetVelocity(self.clientID,self.boom,0,sim.simx_opmode_oneshot)
-            ##implement code to calculate the angle
-            ##value to circle = v and 0 
-            ##self.currentAngleClaw
-            self.convertDistAngle(step)
-
+            self.getCurrentAngleClaw()
+            next_position = self.currentAngleClaw + step
+            sim.simxSetJointTargetPosition(self.clientID,self.boom,degree2radian(next_position),sim.simx_opmode_continuous)
         return self.craneStatus
 
+    def commandRight(self, step=1):
+        return self.actionBoom(step*-1)
+
+    def commandLeft(self, step=1):
+        return self.actionBoom(step)
+
+    # Atuador gancho
     def actionCrane(self, step):
         if self.craneStatus and self.connectionStatus:
-            step *= 0.1
-            sim.simxSetJointTargetVelocity(self.clientID,self.crane,step,sim.simx_opmode_oneshot)
-            time.sleep(0.2)
-            sim.simxSetJointTargetVelocity(self.clientID,self.crane,0,sim.simx_opmode_oneshot)
+            self.getCurrentHeightHook()
+            next_position = self.currentHeightHook + step - 6.6747 # (subtrai offset de altura)
+            sim.simxSetJointTargetPosition(self.clientID,self.crane,next_position,sim.simx_opmode_continuous)
         return self.craneStatus
 
+    def commandUp(self, step=1):
+        return self.actionCrane(step)
+
+    def commandDown(self, step=1):
+        return self.actionCrane(step*-1)
+
+    ######## TODO
     def actionClaw(self, step):
         if self.craneStatus and self.connectionStatus:
             step *= 0.1
@@ -83,41 +125,21 @@ class Connection(object):
             sim.simxSetJointTargetVelocity(self.clientID, self.claw,0,sim.simx_opmode_oneshot)
         return self.craneStatus
 
-    def commandUp(self, step=1):
-        return self.actionCrane(step)
-
-    def commandDown(self, step=1):
-        stepDown = step*-1
-        return self.actionCrane(stepDown)
-    
-    def commandRight(self, step=1):
-        stepLeft = step*-1
-        status = self.actionBoom(stepLeft)
-        print(status)
-        return status
-
-    def commandLeft(self, step=1):
-        return self.actionBoom(step)
-    
-    def commandCraneOnOff(self):
-        if self.connectionStatus:
-            self.craneStatus= not self.craneStatus
-        return self.craneStatus
-
-    def commandMagnetOnOff(self):
-        if self.craneStatus and self.connectionStatus:
-            sim.simxCallScriptFunction(self.clientID,'Base',sim.sim_scripttype_childscript,'AtuadorIma',[],[],[],bytearray(),sim.simx_opmode_blocking)
-            self.magnetStatus = not self.magnetStatus
-        return self.magnetStatus
-
     def commandClawUp(self, step=1):
-        return self.actionBoom(step)
+        return self.actionClaw(step)
     
     def commandClawDown(self, step):
         return self.actionClaw(step*-1)
     
+    # Captura o angulo do braco
     def getCurrentAngleClaw(self):
+        self.currentAngleClaw = radian2degree(sim.simxGetObjectOrientation(self.clientID,self.boomStructure,-1,sim.simx_opmode_blocking)[-1][-1])
         return self.currentAngleClaw
+
+    # Captura a altura do gancho
+    def getCurrentHeightHook(self):
+        self.currentHeightHook = sim.simxGetObjectPosition(self.clientID,self.magnet,-1,sim.simx_opmode_blocking)[-1][-1]
+        return self.currentHeightHook
     
     #no implemention yet
     def getCamImage(self, plot = False, save = False):
--- gui.py
+++ gui.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from connection import Connection
+import time
 # Form implementation generated from reading ui file 'C:\Users\JB\Documents\lab_3\guindaste.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
 # WARNING! All changes made in this file will be lost!
+#
+# Convertido para PyQt5
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -28,36 +33,59 @@ class Ui_Guindaste(object):
         #create connection
         control = Connection()
 
+        # Cores da GUI
+        palette = QtGui.QPalette()
+        palette.setColor(QtGui.QPalette.Background, QtGui.QColor(225,230,245))
+        palette.setColor(QtGui.QPalette.Light, QtGui.QColor(190,190,235))
+
+        # Janela principal da GUI
         Guindaste.setObjectName(_fromUtf8("Guindaste"))
         Guindaste.resize(875, 650)
+        Guindaste.setPalette(palette)
+        Guindaste.setFont(QtGui.QFont("Helvetica"))
 
         # Titulo central "Controle do Guindaste"
         self.label_5 = QtWidgets.QLabel(Guindaste)
-        self.label_5.setGeometry(QtCore.QRect(250, 10, 380, 41))
-        self.label_5.setStyleSheet(_fromUtf8("font: 18pt \"MS Shell Dlg 2\";"))
+        self.label_5.setGeometry(QtCore.QRect(245, 10, 400, 41))
+        self.label_5.setStyleSheet(_fromUtf8("font: 18pt \"Helvetica\";"))
         self.label_5.setObjectName(_fromUtf8("label_5"))
 
         # Titulo esquerdo superior "Estado do Guindaste"
         self.label_6 = QtWidgets.QLabel(Guindaste)
         self.label_6.setGeometry(QtCore.QRect(10, 50, 221, 41))
-        self.label_6.setStyleSheet(_fromUtf8("font: 14pt \"MS Shell Dlg 2\";"))
+        self.label_6.setStyleSheet(_fromUtf8("font: 14pt \"Helvetica\";"))
         self.label_6.setObjectName(_fromUtf8("label_6"))
+
+        # Imagem guindaste
+        self.pixmap = QtGui.QPixmap('crane.png')
+        self.label_13 = QtWidgets.QLabel(Guindaste)
+        self.label_13.setPixmap(self.pixmap)
+        self.label_13.setGeometry(QtCore.QRect(30, 100, 170, 230))
+        self.label_13.setScaledContents(True)
+
+        #lcd colors
+        self.off_color = QtGui.QPalette()
+        self.off_color.setColor(QtGui.QPalette.Light, QtGui.QColor(255,145,145))
+        self.on_color = QtGui.QPalette()
+        self.on_color.setColor(QtGui.QPalette.Light, QtGui.QColor(145,225,145))
 
         # LCDs superiores esquerdos
         self.lcdNumber_4 = QtWidgets.QLCDNumber(Guindaste)
-        self.lcdNumber_4.setGeometry(QtCore.QRect(113.75, 120, 60, 30))
+        self.lcdNumber_4.setGeometry(QtCore.QRect(180, 170, 60, 30))
         self.lcdNumber_4.setObjectName(_fromUtf8("lcdNumber_4"))
+        self.lcdNumber_4.setPalette(self.off_color)
 
         self.label_10 = QtWidgets.QLabel(Guindaste)
-        self.label_10.setGeometry(QtCore.QRect(83.75, 90, 121, 31))
+        self.label_10.setGeometry(QtCore.QRect(150, 140, 121, 31))
         self.label_10.setObjectName(_fromUtf8("label_10"))
 
         self.lcdNumber_5 = QtWidgets.QLCDNumber(Guindaste)
-        self.lcdNumber_5.setGeometry(QtCore.QRect(113.75, 190, 60, 30))
+        self.lcdNumber_5.setGeometry(QtCore.QRect(180, 240, 60, 30))
         self.lcdNumber_5.setObjectName(_fromUtf8("lcdNumber_5"))
+        self.lcdNumber_5.setPalette(self.off_color)
 
         self.label_12 = QtWidgets.QLabel(Guindaste)
-        self.label_12.setGeometry(QtCore.QRect(83.75, 160, 131, 31))
+        self.label_12.setGeometry(QtCore.QRect(150, 210, 131, 31))
         self.label_12.setObjectName(_fromUtf8("label_12"))
 
         # Tela central
@@ -71,7 +99,7 @@ class Ui_Guindaste(object):
         self.lcdNumber_2.setObjectName(_fromUtf8("lcdNumber_2"))
 
         self.label_7 = QtWidgets.QLabel(Guindaste)
-        self.label_7.setGeometry(QtCore.QRect(660, 90, 141, 31))
+        self.label_7.setGeometry(QtCore.QRect(660, 90, 160, 31))
         self.label_7.setObjectName(_fromUtf8("label_7"))
 
         self.lcdNumber = QtWidgets.QLCDNumber(Guindaste)
@@ -85,6 +113,7 @@ class Ui_Guindaste(object):
         self.lcdNumber_6 = QtWidgets.QLCDNumber(Guindaste)
         self.lcdNumber_6.setGeometry(QtCore.QRect(701.25, 260, 60, 30))
         self.lcdNumber_6.setObjectName(_fromUtf8("lcdNumber_6"))
+        self.lcdNumber_6.setPalette(self.off_color)
 
         self.label_8 = QtWidgets.QLabel(Guindaste)
         self.label_8.setGeometry(QtCore.QRect(660, 230, 131, 31))
@@ -92,8 +121,8 @@ class Ui_Guindaste(object):
 
         # Titulo esquerdo "Controles do Guindaste"
         self.label_2 = QtWidgets.QLabel(Guindaste)
-        self.label_2.setGeometry(QtCore.QRect(10, 330, 241, 41))
-        self.label_2.setStyleSheet(_fromUtf8("font: 14pt \"MS Shell Dlg 2\";"))
+        self.label_2.setGeometry(QtCore.QRect(10, 333, 241, 41))
+        self.label_2.setStyleSheet(_fromUtf8("font: 14pt \"Helvetica\";"))
         self.label_2.setObjectName(_fromUtf8("label_2"))
 
         # Quadro inferior esquerdo "Controle do Guindaste"
@@ -101,13 +130,13 @@ class Ui_Guindaste(object):
         self.groupBox_4.setGeometry(QtCore.QRect(10, 380, 181, 151))
         self.groupBox_4.setObjectName(_fromUtf8("groupBox_4"))
 
-        self.pushButton_2 = QtWidgets.QPushButton(self.groupBox_4)
-        self.pushButton_2.setGeometry(QtCore.QRect(50, 90, 80, 30))
-        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
-
         self.pushButton_7 = QtWidgets.QPushButton(self.groupBox_4)
         self.pushButton_7.setGeometry(QtCore.QRect(50, 35, 80, 30))
         self.pushButton_7.setObjectName(_fromUtf8("pushButton_7"))
+
+        self.pushButton_2 = QtWidgets.QPushButton(self.groupBox_4)
+        self.pushButton_2.setGeometry(QtCore.QRect(50, 90, 80, 30))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
 
         # Quadro inferior esquerdo "Controle do imã"
         self.groupBox_3 = QtWidgets.QGroupBox(Guindaste)
@@ -166,8 +195,26 @@ class Ui_Guindaste(object):
         self.verticalSlider.setObjectName(_fromUtf8("verticalSlider"))
 
         self.label_4 = QtWidgets.QLabel(self.groupBox_2)
-        self.label_4.setGeometry(QtCore.QRect(140, 30, 100, 30))
+        self.label_4.setGeometry(QtCore.QRect(140, 30, 120, 30))
         self.label_4.setObjectName(_fromUtf8("label_4"))
+
+        # Janelas de confirmacao
+        self.confirmWindow = QtWidgets.QMessageBox()
+        self.confirmWindow.setIcon(QtWidgets.QMessageBox.Question)
+        self.confirmWindow.setWindowTitle('Controle do Imã')
+        self.confirmWindow.setText('Desligar imã?')
+        self.confirmWindow.setStandardButtons(QtWidgets.QMessageBox.Yes|QtWidgets.QMessageBox.No)
+        self.confirmWindow.setDefaultButton(QtWidgets.QMessageBox.No)
+        self.buttonY = self.confirmWindow.button(QtWidgets.QMessageBox.Yes)
+        self.buttonY.setText('Sim')
+        self.buttonN = self.confirmWindow.button(QtWidgets.QMessageBox.No)
+        self.buttonN.setText('Não')
+
+        self.confirmWindow_2 = QtWidgets.QMessageBox()
+        self.confirmWindow_2.setIcon(QtWidgets.QMessageBox.Question)
+        self.confirmWindow_2.setWindowTitle('Controle do Guindaste')
+        self.confirmWindow_2.setText("AVISO: Imã ligado.\nDesligue-o antes de concluir esta ação.")
+        self.confirmWindow_2.setStandardButtons(QtWidgets.QMessageBox.Ok)
 
 
         self.groupBox_4.raise_()
@@ -201,56 +248,78 @@ class Ui_Guindaste(object):
             _, status = control.init_connection(ip = '127.0.0.1' , port = 19997)
             if status:
                 self.lcdNumber_4.display('UP')
+                self.lcdNumber_4.setPalette(self.on_color)
             else:
                 self.lcdNumber_4.display('Off')
+                self.lcdNumber_4.setPalette(self.off_color)
+
         self.pushButton_7.clicked.connect(connect)
         
-        #left
+        # Transforma os algulos negativos para positivos
+        def getAngle():
+            angle = control.getCurrentAngleClaw()
+            if angle < 0:
+                angle += 360
+            return angle
+
+        # Left
         def left():
             #try catch no int
             cod = self.lineEdit.text()
             try:
-                step = int(cod)
-                print(step)
+                old_angle = getAngle()
+                step = float(cod)
                 status = control.commandLeft(step)
             except:
                 status = False
             if status:
-                angle = control.getCurrentAngleClaw()
-                self.lcdNumber_2.display(angle)
+                desired_angle = old_angle + step
+                cnt = 0
+                angle = getAngle()
+                while(abs(angle - desired_angle) > 0.05 and cnt < 500):
+                    self.lcdNumber_2.display(round(angle))
+                    app.processEvents()
+                    angle = getAngle()
+                    cnt+=1
             else:
                 print('Não pode ser alterado por não estar ligado ou Falta valor de passo no campo acima')
 
         self.pushButton.clicked.connect(left)
 
-        #Right
+        # Right
         def right():
             cod = self.lineEdit.text()
             try:
-                step = int(cod)
-                print(step)
+                old_angle = getAngle()
+                step = float(cod)
                 status = control.commandRight(step)
             except:
                 status = False
             if status:
-                angle = control.getCurrentAngleClaw()
-                self.lcdNumber_2.display(angle)
+                desired_angle = old_angle - step
+                cnt = 0
+                angle = getAngle()
+                while(abs(angle - desired_angle) > 0.05 and cnt < 500):
+                    self.lcdNumber_2.display(round(angle))
+                    app.processEvents()
+                    angle = getAngle()
+                    cnt+=1
             else:
                 print('Não pode ser alterado por não estar ligado ou Falta valor de passo no campo acima')
 
         self.pushButton_4.clicked.connect(right)
 
-        #Up
+        # Up
         def Up():
             cod = self.lineEdit.text()
             try:
-                step = int(cod)
-                print(step)
+                step = float(cod)
                 status = control.commandUp(step)
             except:
                 status = False 
             
             if status:
+
                 dist = control.getStatusDist()
                 self.lcdNumber.display(dist)
             else:
@@ -258,12 +327,11 @@ class Ui_Guindaste(object):
 
         self.pushButton_5.clicked.connect(Up)
 
-        #Down
+        # Down
         def Down():
             cod = self.lineEdit.text()
             try:
-                step = int(cod)
-                print(step)
+                step = float(cod)
                 status = control.commandDown(step)
             except:
                 status = False
@@ -275,28 +343,53 @@ class Ui_Guindaste(object):
                 print('Não pode ser alterado por não estar ligado ou Falta valor de passo no campo acima')
         self.pushButton_3.clicked.connect(Down)
 
-        #magnet
+        # Ima
         def magnet():
-            status = control.commandMagnetOnOff()
-            if status:
-                self.lcdNumber_6.display('UP')
+            if control.craneStatus and control.connectionStatus:
+                status = control.getMagnetStatus()
+                if status:
+                    self.confirmWindow.exec_()
+                    if self.confirmWindow.clickedButton() == self.buttonY:
+                        control.commandMagnetOnOff()
+                        print('Imã Desligado')
+                        self.lcdNumber_6.display('Off')
+                        self.lcdNumber_6.setPalette(self.off_color)
+                else:
+                    control.commandMagnetOnOff()
+                    print('Imã Ligado')
+                    self.lcdNumber_6.display('UP')
+                    self.lcdNumber_6.setPalette(self.on_color)
             else:
-                print('Desligado')
-                self.lcdNumber_6.display('Off')
+                print('Sem conexão ou guindaste desligado.')
         self.pushButton_6.clicked.connect(magnet)
 
-        #on off Crane
+        # On/Off Crane
         def crane():
-            status = control.commandCraneOnOff()
-            if status:
-                print('Ligado')
-                self.lcdNumber_5.display('UP')
-                dist = control.getStatusDist()
-                self.lcdNumber.display(dist)
+            if control.connectionStatus:
+                status = control.getCraneStatus()
+                if status:
+                    if control.getMagnetStatus():
+                        self.confirmWindow_2.exec_()
+                        print("Não é possível desligar o guindaste com o imã ainda ligado.")
+                    else:
+                        print('Desligado')
+                        control.commandCraneOnOff()
+                        self.lcdNumber_5.display('OFF')
+                        self.lcdNumber_5.setPalette(self.off_color)
+                        self.lcdNumber_2.display(0)
+                else:
+                    print('Ligado')
+                    control.commandCraneOnOff()
+                    self.lcdNumber_5.display('UP')
+                    self.lcdNumber_5.setPalette(self.on_color)
+                    
+                    angle = getAngle()
+                    self.lcdNumber_2.display(round(angle))
+                        
+                    dist = control.getStatusDist()
+                    self.lcdNumber.display(dist)
             else:
-                self.lcdNumber_5.display('OFF')
-                print('Desligado')
-
+                print('Sem conexão.')
         self.pushButton_2.clicked.connect(crane)
 
         self.retranslateUi(Guindaste)
@@ -307,7 +400,7 @@ class Ui_Guindaste(object):
         self.label.setText(_translate("Guindaste", "Distância para o piso:", None))
         self.label_2.setText(_translate("Guindaste", "Controles do Guindaste:", None))
         self.label_4.setText(_translate("Guindaste", "Altura do Guincho", None))
-        self.label_5.setText(_translate("Guindaste", "CONTROLE DO GUINDASTE", None))
+        self.label_5.setText(_translate("Guindaste", "GUINDASTE TELEOPERADO", None))
         self.label_6.setText(_translate("Guindaste", "Estado do Guindaste:", None))
         self.label_7.setText(_translate("Guindaste", "Ângulo da Lança [Graus]:", None))
         self.label_8.setText(_translate("Guindaste", "Estado do imã:", None))
@@ -326,7 +419,6 @@ class Ui_Guindaste(object):
         self.pushButton_7.setText(_translate("Guindaste", "Conectar", None))
         self.label_10.setText(_translate("Guindaste", "Estado de Conexão:", None))
         self.label_12.setText(_translate("Guindaste", "Estado do Guindaste:", None))
-    
 
 
 if __name__ == "__main__":
